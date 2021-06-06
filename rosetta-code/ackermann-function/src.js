5ebd0315736897a04297175cab4ddee3af749343
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2021-06-06 19:29:18 +0430

    add rosetta code - 7

---
+++ rosetta-code/ackermann-function/src.js
@@ -0,0 +1,15 @@
+function ack(m, n) {
+
+  if (m == 0) {
+    return n + 1;
+  }
+
+  if (m > 0 && n == 0) {
+    return ack(m - 1 , 1)
+  }
+
+  if (m > 0 && n > 0) {
+    return ack(m - 1, ack(m, n - 1))
+  }
+
+}
